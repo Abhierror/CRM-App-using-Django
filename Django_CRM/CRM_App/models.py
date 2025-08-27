c77421ca318ef3369f2a2c216a7cf512c849ea8e
Abhierror <abhialmalcool@gmail.com>
2025-08-27 17:32:56 +0530

    Add files via upload

---
+++ Django_CRM/CRM_App/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100, unique=True)
+    phone = models.CharField(max_length=10)
+    address = models.CharField(max_length=250)
+    city = models.CharField(max_length=30)
+    zipcode = models.CharField(max_length=10)
+
+    def __str__(self):
+        return(f"{self.first_name} {self.last_name}")
+
+
